d70b9eff6f783e360788db554201745628383d54
Vladimir Stalmakov <akummail@gmail.com>
2015-08-17 22:40:08 +0300

    Добавил табы

--- js/ei.js
+++ js/ei.js
@@ -8,8 +8,8 @@ function CalculateDeposit(percent_In_Year, mount, capitalization, first_payment,
     var percent_In_Mount = percent_In_Year / 12;
     var total = first_payment;
     var total_interest = 0;
-	var all_made = first_payment;
-	
+    var all_made = first_payment;
+
     var result = new Object();
     console.log("Сумма на момент открытия вклада: ", total, " процент по вкладу: ", percent_In_Year, "%", "Ежемесячная процентная ставка:", percent_In_Mount, "%", "капитализация :", capitalization);
     result.pay_percents = new Array();
@@ -19,15 +19,15 @@ function CalculateDeposit(percent_In_Year, mount, capitalization, first_payment,
     });
     for (var i = 1; i <= mount; i++) {
         var pay_percent = (capitalization ? total : first_payment) / 100 * percent_In_Mount;
-		total_interest += pay_percent;
+        total_interest += pay_percent;
         total = total + pay_percent;
         console.log("Платеж по процентам в ", i, " месяце равен ", pay_percent)
-        
+
         if (mount > 1) {
             total += add_summ;
-			all_made+= add_summ;
+            all_made += add_summ;
         }
-		result.pay_percents.push({
+        result.pay_percents.push({
             'mount': i,
             'total': total.toFixed(2)
         });
