146cdf1ab1dcf2e06b0201656cd9c706dcc36eb6
Dmitry Leonov <dimonstar67rus@gmail.com>
2015-09-28 00:41:59 +0300

    Сделал добавление категорий

--- js/ct.js
+++ js/ct.js
@@ -1,42 +1,53 @@
 $(function() {
 
+	
 	//namespace
-	var spends = {}
-	spends.init = {}
-	spends.init.db={}
+	var spendsDataManager = {}
+	spendsDataManager.init = {}
+	spendsDataManager.db={}
+	
 
 	// Для удобства помещаем функцию в глобальную переменную
-	spends.init.open = function(){
-		spends.init.db = openDatabase("spendsDB","1.0","",1024*1024*5);
+	spendsDataManager.openDB = function(){
+		spendsDataManager.db = openDatabase("spendsDB","1.0","",1024*1024*5);
 		// название БД, версия, описание, размер
 	}
 
-	spends.init.createSpendsTable = function(){
-		spends.init.db.transaction(function(tx){
-			tx.executeSql("CREATE TABLE IF NOT EXISTS spends (spend_id int NOT NULL PRIMARY KEY, date Date,sum float,description text, category_id REFERENCES categories(category_id))");
+	spendsDataManager.createSpendsTable = function(){
+		spendsDataManager.db.transaction(function(tx){
+			tx.executeSql("CREATE TABLE IF NOT EXISTS spends (spend_id INTEGER PRIMARY KEY ASC, date Date,sum float,description text, category_id REFERENCES categories(category_id))");
 		});
 	}
 
-	spends.init.createCategoriesTable = function(){
-		spends.init.db.transaction(function(tx){
-			tx.executeSql("CREATE TABLE IF NOT EXISTS categories (category_id int NOT NULL PRIMARY KEY, category text)");
+	spendsDataManager.createCategoriesTable = function(){
+		spendsDataManager.db.transaction(function(tx){
+			tx.executeSql("CREATE TABLE IF NOT EXISTS categories (category_id INTEGER PRIMARY KEY ASC, category text)");
 		});
 	}
 
-	spends.init.setDefaultCategories = function(){
-		spends.init.db.transaction(function(tx){
-			tx.executeSql("INSERT INTO categories (category_id,category) VALUES (1,'Продукты')");
+	spendsDataManager.setTestCategories = function(){
+		spendsDataManager.db.transaction(function(tx){
+			tx.executeSql("INSERT INTO categories (category) VALUES ('продукты')");
 		});
 	}
 
-	spends.init.setTestSpends = function(){
-		spends.init.db.transaction(function(tx){
+	spendsDataManager.setTestSpends = function(){
+		spendsDataManager.db.transaction(function(tx){
 			tx.executeSql("INSERT INTO spends (spend_id,date,sum,description,category_id) VALUES (1,20150924,500.0,'покупочки',1)");
 		});
 	}
+		
+	spendsDataManager.init = function(){
+		spendsDataManager.openDB();
+		spendsDataManager.createCategoriesTable();
+		spendsDataManager.createSpendsTable();	
+		spendsDataManager.setTestCategories();
+		spendsDataManager.setTestSpends();		
+	}
+	
 
 	// Календарь для выбора даты
-	$("#section_expense_report").find("input[name='spendDate']").datepicker({
+	$("#spendDate").datepicker({
 		todayBtn: true,
 		language: "ru",
 		autoclose: true,
@@ -44,10 +55,15 @@ $(function() {
 		toggleActive: true
 	});
 
-	spends.init.open();
-	spends.init.createCategoriesTable();
-	spends.init.createSpendsTable();	
-	spends.init.setDefaultCategories();
-	spends.init.setTestSpends();
+	$("#addCategory_button").click(function(){
+		
+		var newCategory = prompt("Название категории: ", "");
+				spendsDataManager.db.transaction(function(tx){
+			tx.executeSql("INSERT INTO categories (category) VALUES (?)",[newCategory]);
+		});
+		
+	})
+	
+	spendsDataManager.init();
 
 });
