72109f563af18f14d81387ac60a85f4d019d1587
Vladimir Stalmakov <akummail@gmail.com>
2016-02-06 15:34:27 +0300

    Причесал ui скрипт

--- js/ct/ui.js
+++ js/ct/ui.js
@@ -1,110 +1,111 @@
-	var spendsUI = {}
-	spendsUI.init = {}
+var spendsUI = {
+    init: function () {
+        this.context = $("#section_expense_report");
+        this.setCategories();
+        this.setDatepicker();
+        this.setRecentSpends();
 
-	spendsUI.fillCategoriesSelect = function(categories){
+        this.context.find("button[name='addCategoryButton']").bind("click", this.showCategoryAddModal);
+        $("#addCategoryModal").find("button[name='addCategory']").bind("click", this.addCategoryFromModal);
+        this.context.find("button[name='addSpendButton']").bind("click", this.addSpend);
+    },
 
-		$("#section_expense_report").find("select[name='spendСategory']").empty().append(
-			$('<option selected="selected" disabled="disabled">-Select category-</option>')
-		);
+    fillCategoriesSelect: function (categories) {
+        spendsUI.context.find("select[name='spendСategory']").empty().append(
+            $('<option selected="selected" disabled="disabled">-Select category-</option>')
+        );
 
-		for (var i=0; i < categories.length; i++) {
-			$("#section_expense_report").find("select[name='spendСategory']").append(
-				$('<option value="'+categories[i].id+'">'+categories[i].categoryName+'</option>')
-			);
-		}
-	}
+        for (var i = 0; i < categories.length; i++) {
+            $("#section_expense_report").find("select[name='spendСategory']").append(
+                $('<option value="' + categories[i].id + '">' + categories[i].categoryName + '</option>')
+            );
+        }
+    },
 
-	spendsUI.fillRecentSpends = function(spends){
-		$("#section_expense_report").find("#showSpendForm").empty().append(
-        			$('<table class="table" id="spendsTable"><tr><td> id </td><td> дата </td><td> сумма </td><td> категория </td><td> описание </td></tr></table>')
-        		);
+    unixTimeToString: function (time) {
+        //Ущербный код ))) очень)))
+        var d = new Date(time * 1000)
+        var yyyy = d.getFullYear()
+        var mm = ('0' + (d.getMonth() + 1)).slice(-2)
+        var dd = ('0' + d.getDate()).slice(-2)
+        return dd + '.' + mm + '.' + yyyy;
+    },
 
-        		for (var i=0; i < spends.length; i++) {
-        		//Ущербный код
-        		var d = new Date(spends[i].spendDate*1000)
-        		var yyyy = d.getFullYear()
-        		var mm = ('0' + (d.getMonth() + 1)).slice(-2)
-        		var dd = ('0' + d.getDate()).slice(-2)
+    fillRecentSpends: function (spends) {
+        spendsUI.context.find("#showSpendForm").empty().append(
+            $('<table class="table" id="spendsTable"><tr><td> id </td><td> дата </td><td> сумма </td><td> категория </td><td> описание </td></tr></table>')
+        );
 
-        			$("#section_expense_report").find("#spendsTable").append(
-        				'<tr><td> '+spends[i].id+' </td><td> '+dd+'.'+mm+'.'+yyyy+' </td><td> '+spends[i].sum+' </td><td> '+spends[i].category_id+' </td><td> '+spends[i].description+' </td></tr>'
-        			);
-        		};
-	}
+        for (var i = 0; i < spends.length; i++) {
+            spendsUI.context.find("#spendsTable").append(
+                '<tr><td> ' + spends[i].id + ' </td><td> ' +
+                spendsUI.unixTimeToString(spends[i].spendDate) + ' </td><td> ' + spends[i].sum + ' </td><td> ' + spends[i].category_id + ' </td><td> ' + spends[i].description + ' </td></tr>'
+            );
+        }
+    },
 
-	spendsUI.setCategories = function(){
-		spendsDataManager.getCategories(spendsUI.fillCategoriesSelect);
-	}
+    setCategories: function () {
+        spendsDataManager.getCategories(this.fillCategoriesSelect);
+    },
 
-	spendsUI.setRecentSpends = function() {
-		spendsDataManager.getSpends(0,0,spendsUI.fillRecentSpends);
-	}
-	
-	spendsUI.setDatepicker = function(){
-	// Календарь для выбора даты
-	$("#section_expense_report").find("input[name='spendDate']").datepicker({
-		format: 'mm/dd/yyyy',
-		todayBtn: true,
-		language: "ru",
-		autoclose: true,
-		todayHighlight: true,
-		toggleActive: true
-	});
-	
-	}
+    setRecentSpends: function () {
+        spendsDataManager.getSpends(0, 0, this.fillRecentSpends);
+    },
 
-	spendsUI.showCategoryAddModal = function(){
-		// Вызов модального окна
-		$("#addCategoryModal").modal();
-	}
-		
-	spendsUI.addCategoryFromModal = function(){
-		// Метод добавления категории из модального окна
-		var newCategory = $("#addCategoryModal").find("input[name='newCategoryName']").val();
-		if (newCategory != null)
-		{
-			spendsDataManager.db.transaction(function(tx){
-				tx.executeSql("INSERT INTO categories (category) VALUES (?)",[newCategory]);
-			});
-			alert("Добавлено: " + newCategory+"!");				
-		}				
-		$("#addCategoryModal").modal('hide');
-		spendsUI.setCategories();
-	}
+    setDatepicker: function () {
+        var datapicker = this.context.find("input[name='spendDate']");
+        datapicker.datepicker({
+            format: 'mm/dd/yyyy',
+            todayBtn: true,
+            language: "ru",
+            autoclose: true,
+            todayHighlight: true,
+            toggleActive: true
+        });
+        datapicker.datepicker("setDate", new Date());
+    },
 
-	spendsUI.addSpend = function(){
-		var date = Date.parse($("#section_expense_report").find($("input[name='spendDate']")).val())/1000;
-		var sum = $("#section_expense_report").find($("input[name='spendSum']")).val();
-		var category= $("#section_expense_report").find($("select[name='spendСategory']")).find(":selected").val();
-		var description =  $("#section_expense_report").find($("input[name='spendDescription']")).val();
+    showCategoryAddModal: function () {
+        // Вызов модального окна
+        $("#addCategoryModal").modal();
+    },
 
-		if (!date || !sum || !category)
-		{
-			$("#section_expense_report").find($("input[name='spendDate']")).css('border-color','red');
-            $("#section_expense_report").find($("input[name='spendSum']")).css('border-color','red');
-            $("#section_expense_report").find($("select[name='spendСategory']")).css('border-color','red');
+    addCategoryFromModal: function () {
+        // Метод добавления категории из модального окна
+        var newCategory = $("#addCategoryModal").find("input[name='newCategoryName']").val();
+        if (newCategory != null) {
+            spendsDataManager.db.transaction(function (tx) {
+                tx.executeSql("INSERT INTO categories (category) VALUES (?)", [newCategory]);
+            });
+            alert("Добавлено: " + newCategory + "!");
+        }
+        $("#addCategoryModal").modal('hide');
+        spendsUI.setCategories();
+    },
+
+    addSpend: function () {
+        var date = Date.parse(spendsUI.context.find($("input[name='spendDate']")).val()) / 1000;
+        var sum = spendsUI.context.find($("input[name='spendSum']")).val();
+        var category = spendsUI.context.find($("select[name='spendСategory']")).find(":selected").val();
+        var description = spendsUI.context.find($("input[name='spendDescription']")).val();
+
+        if (!date || !sum || !category) {
+            spendsUI.context.find($("input[name='spendDate']")).css('border-color', 'red');
+            spendsUI.context.find($("input[name='spendSum']")).css('border-color', 'red');
+            spendsUI.context.find($("select[name='spendСategory']")).css('border-color', 'red');
             alert("Заполните все поля!");
-			return;
-		}
+            return;
+        }
 
-		spendsDataManager.addSpend(date,sum,category,description);
+        spendsDataManager.addSpend(date, sum, category, description);
 
-		$("#section_expense_report").find($("input[name='spendDate']")).css('border-color','');
-        $("#section_expense_report").find($("input[name='spendSum']")).css('border-color','');
-        $("#section_expense_report").find($("select[name='spendСategory']")).css('border-color','');
+        spendsUI.context.find($("input[name='spendDate']")).css('border-color', '');
+        spendsUI.context.find($("input[name='spendSum']")).css('border-color', '');
+        spendsUI.context.find($("select[name='spendСategory']")).css('border-color', '');
 
-		$("#section_expense_report").find($("input[name='spendDate']")).val('');
-  		$("#section_expense_report").find($("input[name='spendSum']")).val('');
-   		$("#section_expense_report").find($("select[name='spendСategory'] option")).eq(0).prop('selected',true);
-   		$("#section_expense_report").find($("input[name='spendDescription']")).val('');
-	}
-
-	spendsUI.init = function(){
-		spendsUI.setCategories();
-		spendsUI.setDatepicker();
-		spendsUI.setRecentSpends();
-
-		$("#section_expense_report").find("button[name='addCategoryButton']").bind("click",spendsUI.showCategoryAddModal);
-		$("#addCategoryModal").find("button[name='addCategory']").bind("click",spendsUI.addCategoryFromModal);
-		$("#section_expense_report").find("button[name='addSpendButton']").bind("click",spendsUI.addSpend);
-	}
+        spendsUI.context.find($("input[name='spendDate']")).val('');
+        spendsUI.context.find($("input[name='spendSum']")).val('');
+        spendsUI.context.find($("select[name='spendСategory'] option")).eq(0).prop('selected', true);
+        spendsUI.context.find($("input[name='spendDescription']")).val('');
+    }
+};
